70c374dbad507a335643669fa54b68e33a016549
Paulo <paulo.rqsf@gmail.com>
2022-01-25 19:06:16 -0300

    Adicionados comentarios

--- Exemplo.c
+++ Exemplo.c
@@ -8,12 +8,17 @@ int multi(int a, int b){
     return a*b;
 }
 int main(void){
+    //Variáveis
     int a,b,c;
+
+    //Código
     printf("Hello World!");
     printf("\n\nDigite um valor: ");
     scanf("%d", a);
     printf("\nDigite um segundo valor a ser somado: ");
     scanf("%d", b);
+
+    //Saídas
     printf("\nO resultado da soma de %d com %d é %d",a,b,soma(a,b)); 
     printf("\nE o resultado da multiplicação é %d", multi(a,b));
 }
