4b6927d1a197a2e20d959c02a5eac3be3a63b1f4
Ana <Ana.Silva@gmail.com>
2022-01-25 18:55:25 -0300

    Criada função de multiplicação, e o git ignore

--- Exemplo.c
+++ Exemplo.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int soma(int a, int b){
     return a+b;
+}
+int multi(int a, int b){
+    return a*b;
 }
 int main(void){
     int a,b,c;
@@ -10,6 +14,6 @@ int main(void){
     scanf("%d", a);
     printf("\nDigite um segundo valor a ser somado: ");
     scanf("%d", b);
-    printf("O resultado da soma de %d com %d é %d",a,b,soma(a,b)); 
-    
+    printf("\nO resultado da soma de %d com %d é %d",a,b,soma(a,b)); 
+    printf("\nE o resultado da multiplicação é %d", multi(a,b));
 }
