2e4f8b335b16b70572396efb9232477466ba8735
Paulo <paulo.rqsf@gmail.com>
2022-01-25 18:45:41 -0300

    Adicionada a função de soma, junto com menu para o usuário somar dois números.

--- Exemplo.c
+++ Exemplo.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+int soma(int a, int b){
+    return a+b;
+}
 int main(void){
-
+    int a,b,c;
     printf("Hello World!");
+    printf("\n\nDigite um valor: ");
+    scanf("%d", a);
+    printf("\nDigite um segundo valor a ser somado: ");
+    scanf("%d", b);
+    printf("O resultado da soma de %d com %d é %d",a,b,soma(a,b)); 
+    
 }
